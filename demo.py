7ab0183e34ae061d10a792a10912abbe0a88c9ab
strain-tech <150489117+strain-tech@users.noreply.github.com>
2023-11-10 18:17:32 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,6 +1,24 @@
+import argparse
 import os
 import netmiko
 
-print("Hello World")
-if 'JENKINS_URL' in os.environ:
-  print('Inside Jenkins')
+def init_args():
+  args = get_args()
+  return args
+
+def get_args():
+  parser = argparse.ArgumentParser(description="Demo Automation Project")
+  parser.add_argument("device", help="Device Hostname or IP")
+  args = parser.parse_args()
+  return args
+
+def main():
+  print("Hello World")
+  args = init_args()
+  print("args", args)
+  
+  if 'JENKINS_URL' in os.environ:
+    print('Inside Jenkins')
+
+if __name__ == '__main__':
+  main()
